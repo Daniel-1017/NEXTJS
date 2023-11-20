fcfb9ee722b5baae9c4c6ca8cf11ad9ae28524ec
Frimu Daniel <frimudaniel17@gmail.com>
2023-11-20 16:38:16 +0100

    Navigating programmatically

--- 01_pages_and_file-based_routing/pages/clients/[id]/index.js
+++ 01_pages_and_file-based_routing/pages/clients/[id]/index.js
@@ -1,7 +1,17 @@
+import { useRouter } from 'next/router';
+
 const ClientProjectsPage = () => {
+  const router = useRouter();
+
+  const loadProjectHandler = () => {
+    // load data...
+    router.push('/clients/max/projectA');
+  };
+
   return (
     <div>
       <h1>The Client Projects Page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
