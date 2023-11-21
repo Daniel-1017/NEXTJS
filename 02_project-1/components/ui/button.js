5367eaed68175aa89ac9f81995f93a695aa0fbe3
Frimu Daniel <frimudaniel17@gmail.com>
2023-11-21 18:54:52 +0100

    Adding a filter form for filtering events

--- 02_project-1/components/ui/button.js
+++ 02_project-1/components/ui/button.js
@@ -1,11 +1,19 @@
 import Link from 'next/link';
 import classes from './button.module.css';
 
-const Button = ({ children, link }) => {
+const Button = ({ children, link, onClick }) => {
+  if (link) {
+    return (
+      <Link href={link} className={classes.btn}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
-    <Link href={link} className={classes.btn}>
+    <button className={classes.btn} onClick={onClick}>
       {children}
-    </Link>
+    </button>
   );
 };
 
