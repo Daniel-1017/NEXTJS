64367e01ea18267826c6d7bc0a427abe8913969b
Frimu Daniel <frimudaniel17@gmail.com>
2024-02-24 11:16:15 +0100

    Loading paths dynamically

---
+++ 03_page_pre-rendering_and_data-fetching/pages/[pid].js
@@ -0,0 +1,48 @@
+import path from 'path';
+import fs from 'fs/promises';
+import { Fragment } from 'react';
+
+const ProductDetailPage = ({ loadedProduct }) => {
+  return (
+    <Fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </Fragment>
+  );
+};
+
+const getData = async () => {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
+export const getStaticProps = async context => {
+  const { params } = context;
+  const productId = params.pid;
+
+  const data = await getData();
+
+  const product = data.products.find(product => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const data = await getData();
+
+  const ids = data.products.map(product => product.id);
+  const pathsWithParams = ids.map(id => ({params: {pid: id}}))
+
+  return {
+    paths: pathsWithParams,
+    fallback: false,
+  };
+};
+
+export default ProductDetailPage;
