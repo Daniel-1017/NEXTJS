d0e84303ab2279e0e25fc4fca226095e16b1f7ea
Frimu Daniel <frimudaniel17@gmail.com>
2024-04-21 22:12:19 +0200

    Styling nextjs projects using css modules

--- 01_nextjs_essentials/components/MainHeader.js
+++ 01_nextjs_essentials/components/MainHeader.js
@@ -1,17 +1,18 @@
 import Link from "next/link";
 import React from "react";
+import classes from "./MainHeader.module.css";
 
 import logoImg from "@/assets/logo.png";
 
 const MainHeader = () => {
     return (
-        <header>
-            <Link href="/">
+        <header className={classes.header}>
+            <Link href="/" className={classes.logo}>
                 <img src={logoImg.src} alt="A plate with food on it" />
                 NextLevel Food
             </Link>
 
-            <nav>
+            <nav className={classes.nav}>
                 <ul>
                     <li>
                         <Link href="/meals">Browse Meals</Link>
