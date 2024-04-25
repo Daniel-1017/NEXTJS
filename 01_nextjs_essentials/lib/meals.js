affcd97eb0acedf16a26e99169fd0c235a7b9619
Frimu Daniel <frimudaniel17@gmail.com>
2024-04-25 16:31:28 +0200

    Creating a Slug & Sanitizing User Input for XSS Protection

--- 01_nextjs_essentials/lib/meals.js
+++ 01_nextjs_essentials/lib/meals.js
@@ -1,4 +1,6 @@
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
 
 const db = sql("meals.db");
 
@@ -12,3 +14,8 @@ export const getMeals = async () => {
 
 export const getMeal = slug =>
     db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+
+export const saveMeal = meal => {
+    meal.slug = slugify(meal.slug, { lower: true });
+    meal.instructions = xss(meal.instructions);
+};
