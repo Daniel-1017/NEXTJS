d79da7b9d6092d6d4b47c17c839d0020f301d9a5
Frimu Daniel <frimudaniel17@gmail.com>
2024-04-27 18:22:46 +0200

    Catch-All Fallback Routes & Dealing With Multiple Path Segments

--- 05_routing_and_page_rendering/app/archive/@archive/[[...filter]]/page.js
+++ 05_routing_and_page_rendering/app/archive/@archive/[[...filter]]/page.js
@@ -1,24 +1,55 @@
 import NewsList from "@/components/news-list";
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
 import Link from "next/link";
 
 const FilteredNewsPage = ({ params }) => {
   const { filter } = params;
 
-  const links = getAvailableNewsYears();
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news,
+    links = getAvailableNewsYears(),
+    newsContent = <p>No news found for the selected period.</p>;
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  if (news && news.length > 0) newsContent = <NewsList news={news} />;
 
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map(link => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map(link => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   );
 };
 
