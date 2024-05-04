e0432c996a0460118250d8a0493e6eaa52826fb5
Frimu Daniel <frimudaniel17@gmail.com>
2024-05-04 14:29:21 +0200

    Setting Up Data Caching For Custom Data Sources

--- 08_configuring_caching/app/messages/page.js
+++ 08_configuring_caching/app/messages/page.js
@@ -6,7 +6,7 @@ import { getMessages } from '@/lib/messages';
 // export const revalidate = 5;
 // export const dynamic = "force-dynamic" // default: "auto"
 
-export default function MessagesPage() {
+export default async function MessagesPage() {
   // unstable_noStore(); // it is the same as settings the dynamic variable to "force-dynamic"
   // unstable_noStore, disables caching just in one component
   /* const response = await fetch('http://localhost:8080/messages', {
@@ -18,7 +18,7 @@ export default function MessagesPage() {
   });
   const messages = await response.json(); */
 
-  const messages = getMessages()
+  const messages = await getMessages()
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
