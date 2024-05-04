af1dba964589568389236824db6384fc2caf5226
Frimu Daniel <frimudaniel17@gmail.com>
2024-05-04 13:48:02 +0200

    Controlling Data Caching

--- 08_configuring_caching/app/messages/page.js
+++ 08_configuring_caching/app/messages/page.js
@@ -1,6 +1,13 @@
+import { unstable_noStore } from 'next/cache';
 import Messages from '@/components/messages';
 
+// ! Managing cache for all requests in the file
+// export const revalidate = 5;
+// export const dynamic = "force-dynamic" // default: "auto"
+
 export default async function MessagesPage() {
+  unstable_noStore(); // it is the same as settings the dynamic variable to "force-dynamic"
+  // unstable_noStore, disables caching just in one component
   const response = await fetch('http://localhost:8080/messages', {
     // cache: "no-store" // data will not be cached
     next: {
