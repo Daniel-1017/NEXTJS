54ded4b20ad5149d0cb01e727f21600b3ac5c2a6
Frimu Daniel <frimudaniel17@gmail.com>
2024-05-08 20:39:10 +0200

    Priority settings

--- 09_app_optimization/components/header.js
+++ 09_app_optimization/components/header.js
@@ -12,6 +12,7 @@ export default function Header() {
           // width={100}
           // height={100}
           sizes="10vw" // recommended way of resizing an image
+          priority
           alt="Mobile phone with posts feed on it"
         />
       </Link>
