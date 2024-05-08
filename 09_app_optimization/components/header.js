174ef0dbed78f324e3b13c3a2aa328e8b5cfca7c
Frimu Daniel <frimudaniel17@gmail.com>
2024-05-08 20:37:29 +0200

    Controlling the image size

--- 09_app_optimization/components/header.js
+++ 09_app_optimization/components/header.js
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 import logo from "@/assets/logo.png";
 
@@ -7,7 +7,13 @@ export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <Image src={logo.src} alt="Mobile phone with posts feed on it" />
+        <Image
+          src={logo.src}
+          // width={100}
+          // height={100}
+          sizes="10vw" // recommended way of resizing an image
+          alt="Mobile phone with posts feed on it"
+        />
       </Link>
       <nav>
         <ul>
@@ -15,7 +21,9 @@ export default function Header() {
             <Link href="/feed">Feed</Link>
           </li>
           <li>
-            <Link className='cta-link' href="/new-post">New Post</Link>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
